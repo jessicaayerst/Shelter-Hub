1ea114bed110490a7aba59ef04e1f2558a79f6e7
jessicaayerst@gmail.com <jessicaayerst@gmail.com>
2020-01-29 14:23:18 -0500

    completed CreateClientGroups

--- ShelterHub/Models/Client.cs
+++ ShelterHub/Models/Client.cs
@@ -14,7 +14,7 @@ namespace ShelterHub.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         [NotMapped]
-        public string FullName { get { return FirstName + LastName; } }
+        public string FullName { get { return FirstName  +  LastName; } }
         public string Email { get; set; }
         public string Phone { get; set; }
         public bool OkToText { get; set; }
